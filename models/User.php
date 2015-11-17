a170a7d7e32d5d801ef28e5c877e00884a874f1d
Franklin Alexis Paula Aguirre <franklin.paula@share.com.ec>
2015-11-16 21:17:42 -0500

    not supported

--- models/User.php
+++ models/User.php
@@ -91,7 +91,7 @@ class User extends \yii\db\ActiveRecord implements \yii\web\IdentityInterface
      */
     public function getAuthKey()
     {
-        return $this->auth_key;
+        throw new NotSupportedException('"getAuthKey" is not implemented.');
     }
 
     /**
