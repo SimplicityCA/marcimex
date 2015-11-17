87d8ee23b8d55196ab8ab72e61005e59bfd09339
Franklin Alexis Paula Aguirre <franklin.paula@share.com.ec>
2015-11-16 20:27:46 -0500

    auth key

--- models/User.php
+++ models/User.php
@@ -91,7 +91,7 @@ class User extends \yii\db\ActiveRecord implements \yii\web\IdentityInterface
      */
     public function getAuthKey()
     {
-        return $this->authKey;
+        throw new NotSupportedException('"findIdentityByAccessToken" is not implemented.');
     }
 
     /**
